045bece7f807979ffe0f7b12b17a2d51bf8b6e1e
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-05-14 18:22:20 -0300

    update card detail

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import "../static/css/Card.css";
-import level from '../static/images/level/level2.jpg';
+import level from '../static/images/level/level.jpeg';
 const Card = ({card, detail}) => {
     
   if (!detail) return <div className='card'><img className='imgcard'src={card.card_images[0].image_url} alt={card.name}/></div>
   console.log(card)
   return (
-    <div >
+    <div>
         <img className='detailcard imgcard'src={card.card_images[0].image_url} alt={card.name}/>
         <div className='hide'>
           <h1>{card.name}</h1>
-          <img className='detailcard imglevel'src={level} alt="level"/>
+          <div className='level'>
+            {card.level && <><p className=''>{card.level}</p><img className='detailcard imglevel'src={level} alt="level"/></>}
+          </div>
           <p>{card.atk} {card.def}</p>
           <p>{card.desc}</p>
         </div>
