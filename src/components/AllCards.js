fd872dbd5e72211d2156834130ee9301a18c5899
joao calado <jcaladomoura@gmail.com>
2022-05-15 20:40:26 -0300

    all cards update

--- src/components/AllCards.js
+++ src/components/AllCards.js
@@ -3,9 +3,24 @@ import "../static/css/AllCards.css"
 
 const AllCards = () => {
     return(  
-        <div>
+        <div className='row'>
+            <div className='col-8'>
             <div className='container test'>
-                
+                <div className='title'>
+                    <h1>List of all cards</h1>
+                </div>
+                <div className='container'>
+                <div className='filtros row'>
+                    <div className='col'>
+                        <h2>Filterㅤ</h2>
+                        <button  class="button4">Chose</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className='detail col-4'>
+                box
             </div>
 
         </div>
